b0e59386c22f8be560373e0fda5cd7e5173c3b0a
Darcy McLean <darc.mclean@gmail.com>
2017-10-24 16:23:17 +0000

    Updated code snippets from LHL w4d2

---
+++ test_simple_lookup.js
@@ -0,0 +1,36 @@
+/*
+  LHL w4d2 exercises
+  20171024 DM - argv[2] - last name.
+*/
+console.log(process.argv);
+
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query("SELECT id, first_name, last_name, TO_CHAR(birthdate, 'YYYY-MM-DD') as birthdate FROM famous_people WHERE first_name = $1::varchar OR last_name = $1::varchar", [process.argv[2]], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    //console.log(result.rows[0].first_name); //output: 1
+    let strMsg = '';
+    strMsg += `- ${result.rows[0].id}:`;
+    strMsg += `${result.rows[0].first_name} ${result.rows[0].last_name}, `;
+    strMsg += `born '${result.rows[0].birthdate}'`;
+
+    console.log(strMsg);
+    client.end();
+  });
+});
